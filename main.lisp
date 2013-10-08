62c402b0e5223a087586153d99610faee574d529
Dimitri Fontaine <dim@tapoueh.org>
2013-10-09 00:20:47 +0200

    Improve the main command line facility to match the documentation.

--- main.lisp
+++ main.lisp
@@ -1,22 +1,64 @@
 (in-package #:pgloader)
 
+(defun list-encodings ()
+  "List known encodings names and aliases from charsets::*lisp-encodings*."
+  (format *standard-output* "Name    ~30TAliases~%")
+  (format *standard-output* "--------~30T--------------~%")
+  (loop
+     with encodings = (sort (copy-tree charsets::*lisp-encodings*) #'string<
+			    :key #'car)
+     for (name . aliases) in encodings
+     do (format *standard-output* "~a~30T~{~a~^, ~}~%" name aliases))
+  (terpri))
+
 (defparameter *opt-spec*
-  `((("help" #\h) :type boolean :documentation "show usage")
-    (("file" #\f) :type string :documentation "read commands from file")))
+  `((("help" #\h) :type boolean :documentation "Show usage and exit.")
+
+    (("version" #\V) :type boolean
+     :documentation "Displays pgloader version and exit.")
+
+    (("verbose" #\v) :type boolean :documentation "Be verbose")
+    (("debug"   #\d) :type boolean :documentation "Diplay debug level information.")
+
+    (("list-encodings" #\E) :type boolean
+     :documentation "List pgloader known encodings and exit.")
+
+    (("load" #\l) :type string :list t :optional t
+     :documentation "Read user code from file")))
 
 (defun main (argv)
   "Entry point when building an executable image with buildapp"
   (let ((args (rest argv)))
     (multiple-value-bind (options arguments)
 	(command-line-arguments:process-command-line-options *opt-spec* args)
-      (declare (ignore arguments))
-      (destructuring-bind (&key help file) options
+
+      (destructuring-bind (&key help version verbose debug list-encodings load)
+	  options
+
+	(when version
+	  (format t "pgloader version ~s~%" *version-string*))
 
 	(when help
-	  (command-line-arguments:show-option-help *opt-spec*)
+	  (command-line-arguments:show-option-help *opt-spec*))
+
+	(when (or help version) (uiop:quit))
+
+	(when list-encodings
+	  (list-encodings)
 	  (uiop:quit))
 
-	(run-command (slurp-file-into-string file))
-	(format t "~&")
+	(if debug
+	    (setf *client-min-messages* :debug)
+	    (when verbose
+	      (setf *client-min-messages* :info)))
+
+	(when load
+	  (loop for filename in load
+	     do (load (compile-file filename :verbose nil :print nil))))
+
+	(loop for filename in arguments
+	   do
+	     (run-commands (pathname filename))
+	     (format t "~&"))
 
 	(uiop:quit)))))
