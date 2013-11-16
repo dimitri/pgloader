c809d93b05c3a0170907198f1bcc4b656fd943ec
Dimitri Fontaine <dim@tapoueh.org>
2013-11-16 21:41:42 +0100

    New feature, bump the version number.

--- src/params.lisp
+++ src/params.lisp
@@ -27,7 +27,7 @@
 
 (in-package :pgloader.params)
 
-(defparameter *version-string* "3.0.91"
+(defparameter *version-string* "3.0.92"
   "pgloader version strings, following Emacs versionning model.")
 
 ;; we can't use pgloader.utils:make-pgstate yet because params is compiled
