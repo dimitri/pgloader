d5890f7779eeaef7a1352c3dc14dd2884b718769
Dimitri Fontaine <dim@tapoueh.org>
2013-12-27 12:22:03 +0100

    Release 3.0.96, debian packaging included.

--- src/params.lisp
+++ src/params.lisp
@@ -32,7 +32,7 @@
 
 (in-package :pgloader.params)
 
-(defparameter *version-string* "3.0.95"
+(defparameter *version-string* "3.0.96"
   "pgloader version strings, following Emacs versionning model.")
 
 ;; we can't use pgloader.utils:make-pgstate yet because params is compiled
