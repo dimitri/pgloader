3bcd236de629c50436eab9e5464dcfd74a767587
Dimitri Fontaine <dim@tapoueh.org>
2014-06-03 12:19:23 +0200

    Add automated regression tests.
    Those tests currently only work when a single table is the target of the
    load, and when this target is explicit in the INTO target clause. More
    work needs to be done to cover interesting cases like MySQL and SQLite
    where we want to diff a full database rather than a single table.

---
+++ test/regress.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# regress test driver
+#  - run pgloader on the given .load command file
+#  - parse the PostgreSQL connection string and target table
+#  - output a CSV for the target table
+#  - diff the CSV and error if diffs found
+
+pgloader=$1
+command=$2
+targetdb=`awk -F '[ ?]+' '/^ *INTO|into/ {print $3}' < $command`
+table=`awk -F '[ ?]+' '/^ *INTO|into/ {print $4}' < $command`
+
+expected=regress/expected/`basename $2 .load`.out
+out=regress/out/`basename $2 .load`.out
+
+$pgloader $command
+psql -c "copy $table to stdout" -d "$targetdb" > $out
+diff -c $expected $out
