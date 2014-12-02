dimitri/pgloader

commit f263d1b2a41b8040967a5c5d03351f79beb63d22

Implement Foreign Key support for MSSQL.
Piggyback as much as possible on the work already done for MySQL.

src/sources/mysql.lisp
@@ -216,7 +216,7 @@
     ;; while CREATE INDEX statements are in flight (avoid locking).
     ;;
     (when reset-sequences
-      (reset-pgsql-sequences all-columns :state state))
+      (reset-sequences all-columns :state state))
 
     ;;
     ;; Turn UNIQUE indexes into PRIMARY KEYS now

src/package.lisp
@@ -96,10 +96,13 @@
 	   #:make-pgsql-fkey
 	   #:format-pgsql-create-fkey
 	   #:format-pgsql-drop-fkey
+           #:drop-pgsql-fkeys
+           #:create-pgsql-fkeys
 	   #:make-pgsql-index
 	   #:index-table-name
 	   #:format-pgsql-create-index
-	   #:create-indexes-in-kernel))
+	   #:create-indexes-in-kernel
+           #:reset-sequences))
 
 (defpackage #:pgloader.parse-date
   (:use #:cl #:esrap)
@@ -250,10 +253,13 @@
 		#:make-pgsql-fkey
 		#:format-pgsql-create-fkey
 		#:format-pgsql-drop-fkey
+                #:drop-pgsql-fkeys
+                #:create-pgsql-fkeys
 		#:make-pgsql-index
 		#:format-pgsql-create-index
 		#:create-indexes-in-kernel
-                #:format-vector-row)
+                #:format-vector-row
+                #:reset-sequences)
   (:export #:copy-mysql
 	   #:*mysql-default-cast-rules*
            #:with-mysql-connection
@@ -282,7 +288,8 @@
 		#:make-pgsql-index
 		#:index-table-name
 		#:format-pgsql-create-index
-		#:create-indexes-in-kernel)
+		#:create-indexes-in-kernel
+                #:reset-sequences)
   (:export #:copy-sqlite
            #:*sqlite-default-cast-rules*
 	   #:map-rows
@@ -301,13 +308,22 @@
 		#:pgsql-execute
 		#:pgsql-execute-with-timing
 		#:apply-identifier-case
+		#:list-tables-and-fkeys
+		#:list-table-oids
 		#:create-tables
                 #:truncate-tables
 		#:format-pgsql-column
+		#:format-extra-type
+		#:make-pgsql-fkey
+		#:format-pgsql-create-fkey
+		#:format-pgsql-drop-fkey
+                #:drop-pgsql-fkeys
+                #:create-pgsql-fkeys
 		#:make-pgsql-index
-		#:index-table-name
 		#:format-pgsql-create-index
-		#:create-indexes-in-kernel)
+		#:create-indexes-in-kernel
+                #:format-vector-row
+                #:reset-sequences)
   (:export #:copy-mssql
            #:*mssql-default-cast-rules*
 	   #:map-rows

src/pgsql/schema.lisp
@@ -53,7 +53,6 @@
   nil)
 
 
-
 ;;;
 ;;; API for Foreign Keys
 ;;;
@@ -96,6 +95,34 @@
       ;; we could do that without all-pgsql-fkeys in 9.2 and following with:
       ;; alter table if exists ... drop constraint if exists ...
       (format nil "ALTER TABLE ~a DROP CONSTRAINT ~a" table-name constraint-name))))
+
+(defun drop-pgsql-fkeys (all-fkeys &key (dbname *pg-dbname*))
+  "Drop all Foreign Key Definitions given, to prepare for a clean run."
+  (let ((all-pgsql-fkeys (list-tables-and-fkeys dbname)))
+    (loop for (table-name . fkeys) in all-fkeys
+       do
+	 (loop for fkey in fkeys
+	    for sql = (format-pgsql-drop-fkey fkey
+					      :all-pgsql-fkeys all-pgsql-fkeys)
+	    when sql
+	    do
+	      (log-message :notice "~a;" sql)
+	      (pgsql-execute sql)))))
+
+(defun create-pgsql-fkeys (all-fkeys
+                           &key
+                             (dbname *pg-dbname*)
+                             state
+                             (label "Foreign Keys"))
+  "Actually create the Foreign Key References that where declared in the
+   MySQL database"
+  (pgstate-add-table state dbname label)
+  (loop for (table-name . fkeys) in all-fkeys
+     do (loop for fkey in fkeys
+	   for sql = (format-pgsql-create-fkey fkey)
+	   do
+	     (log-message :notice "~a;" sql)
+	     (pgsql-execute-with-timing dbname "Foreign Keys" sql state))))
 
 
 ;;;
@@ -247,3 +274,17 @@
 
                   ;; return the pkey "upgrade" statement
                   pkey))))
+
+
+;;;
+;;; Sequences
+;;;
+(defun reset-sequences (table-names
+                              &key (dbname *pg-dbname*) state)
+  "Reset all sequences created during this MySQL migration."
+  (log-message :notice "Reset sequences")
+  (with-stats-collection ("Reset Sequences"
+                          :dbname dbname
+                          :use-result-as-rows t
+                          :state state)
+    (reset-all-sequences dbname :tables table-names)))

src/sources/mssql.lisp
@@ -64,7 +64,7 @@
             (lambda (row)
               (pgstate-incf *state* (target mssql) :read 1)
               (funcall process-row-fn row))))
-      (log-message :log "~a" sql)
+      (log-message :debug "~a" sql)
       (handler-case
           (mssql::map-query-results sql :row-fn row-fn :connection *mssql-db*)
         (condition (e)
@@ -106,9 +106,53 @@
     ;; return the copy-mssql object we just did the COPY for
     mssql))
 
+(defun complete-pgsql-database (all-columns all-fkeys pkeys
+                                &key
+                                  state
+                                  data-only
+                                  foreign-keys
+                                  reset-sequences)
+    "After loading the data into PostgreSQL, we can now reset the sequences
+     and declare foreign keys."
+    ;;
+    ;; Now Reset Sequences, the good time to do that is once the whole data
+    ;; has been imported and once we have the indexes in place, as max() is
+    ;; able to benefit from the indexes. In particular avoid doing that step
+    ;; while CREATE INDEX statements are in flight (avoid locking).
+    ;;
+    (when reset-sequences
+      (let ((table-names (mapcar #'car (qualified-table-name-list all-columns))))
+        (reset-sequences table-names  :state state)))
+
+    ;;
+    ;; Turn UNIQUE indexes into PRIMARY KEYS now
+    ;;
+    (pgstate-add-table state *pg-dbname* "Primary Keys")
+    (loop :for sql :in pkeys
+       :when sql
+       :do (progn
+             (log-message :notice "~a" sql)
+             (pgsql-execute-with-timing *pg-dbname* "Primary Keys" sql state)))
+
+    ;;
+    ;; Foreign Key Constraints
+    ;;
+    ;; We need to have finished loading both the reference and the refering
+    ;; tables to be able to build the foreign keys, so wait until all tables
+    ;; and indexes are imported before doing that.
+    ;;
+    (when (and foreign-keys (not data-only))
+      ;; convert to schema-less list of fkeys
+      ;; TODO: fix the MySQL support inherited API
+      (let ((all-fkeys
+             (loop :for (schema . tables) :in all-fkeys
+                :append (loop :for (table-name . fkeys) :in tables
+                           :collect (cons table-name (mapcar #'cdr fkeys))))))
+        (create-pgsql-fkeys all-fkeys :state state))))
+
 (defun fetch-mssql-metadata (&key state only-tables)
   "MS SQL introspection to prepare the migration."
-  (let (all-columns all-indexes)
+  (let (all-columns all-indexes all-fkeys)
     (with-stats-collection ("fetch meta data"
                             :use-result-as-rows t
                             :use-result-as-read t
@@ -120,6 +164,9 @@
         (setf all-indexes (filter-column-list (list-all-indexes)
                                               :only-tables only-tables))
 
+        (setf all-fkeys   (filter-column-list (list-all-fkeys)
+                                              :only-tables only-tables))
+
         ;; return how many objects we're going to deal with in total
         ;; for stats collection
         (+ (loop :for (schema . tables) :in all-columns :sum (length tables))
@@ -128,7 +175,9 @@
                       :sum (length indexes))))))
 
     ;; now return a plist to the caller
-    (list :all-columns all-columns :all-indexes all-indexes)))
+    (list :all-columns all-columns
+          :all-indexes all-indexes
+          :all-fkeys all-fkeys)))
 
 (defmethod copy-database ((mssql copy-mssql)
                           &key
@@ -146,7 +195,6 @@
                             (encoding        :utf-8)
 			    only-tables)
   "Stream the given MS SQL database down to PostgreSQL."
-  (declare (ignore reset-sequences foreign-keys))
   (let* ((summary       (null *state*))
 	 (*state*       (or *state* (make-pgstate)))
 	 (idx-state     (or state-indexes (make-pgstate)))
@@ -156,7 +204,7 @@
          (copy-kernel   (make-kernel 2))
          idx-kernel idx-channel)
 
-    (destructuring-bind (&key all-columns all-indexes)
+    (destructuring-bind (&key all-columns all-indexes all-fkeys pkeys)
         ;; to prepare the run we need to fetch MS SQL meta-data
         (fetch-mssql-metadata :state state-before
                               :only-tables only-tables)
@@ -223,8 +271,9 @@
                                       :source    (cons schema table-name)
                                       :target    (qualify-name schema table-name)
                                       :fields    columns)))
+
                   (log-message :debug "TARGET: ~a" (target table-source))
-                  (log-message :log "target: ~s" table-source)
+
                   ;; COPY the data to PostgreSQL, using copy-kernel
                   (unless schema-only
                     (copy-from table-source :kernel copy-kernel))
@@ -241,11 +290,14 @@
                     (let* ((s-entry  (assoc schema all-indexes :test 'equal))
                            (indexes-with-names
                             (cdr (assoc table-name (cdr s-entry) :test 'equal))))
-                      (create-indexes-in-kernel (target-db mssql)
-                                                (mapcar #'cdr indexes-with-names)
-                                                idx-kernel
-                                                idx-channel
-                                                :state idx-state))))))
+
+                      (alexandria:appendf
+                       pkeys
+                       (create-indexes-in-kernel (target-db mssql)
+                                                 (mapcar #'cdr indexes-with-names)
+                                                 idx-kernel
+                                                 idx-channel
+                                                 :state idx-state)))))))
 
       ;; now end the kernels
       (let ((lp:*kernel* copy-kernel))  (lp:end-kernel))
@@ -256,6 +308,15 @@
           (with-stats-collection ("Index Build Completion" :state *state*)
             (loop for idx in all-indexes do (lp:receive-result idx-channel))))
         (lp:end-kernel))
+
+      ;;
+      ;; Complete the PostgreSQL database before handing over.
+      ;;
+      (complete-pgsql-database all-columns all-fkeys pkeys
+                               :state state-after
+                               :data-only data-only
+                               :foreign-keys foreign-keys
+                               :reset-sequences reset-sequences)
 
       ;; and report the total time spent on the operation
       (when summary

src/sources/mssql/mssql-schema.lisp
@@ -201,6 +201,85 @@ order by SchemaName,
           (loop :for (schema . tables) :in result
              :collect (cons schema (reverse-indexes-cols tables))))))))
 
+(defun list-all-fkeys (&key (dbname *ms-dbname*))
+  "Get the list of MSSQL index definitions per table."
+  (loop
+     :with result := nil
+     :for (name schema table col fschema ftable fcol)
+     :in  (mssql-query (format nil "
+   SELECT
+           KCU1.CONSTRAINT_NAME AS 'CONSTRAINT_NAME'
+         , KCU1.TABLE_SCHEMA AS 'TABLE_SCHEMA'
+         , KCU1.TABLE_NAME AS 'TABLE_NAME'
+         , KCU1.COLUMN_NAME AS 'COLUMN_NAME'
+         , KCU2.TABLE_SCHEMA AS 'UNIQUE_TABLE_SCHEMA'
+         , KCU2.TABLE_NAME AS 'UNIQUE_TABLE_NAME'
+         , KCU2.COLUMN_NAME AS 'UNIQUE_COLUMN_NAME'
+
+    FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS RC
+         JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE KCU1
+              ON KCU1.CONSTRAINT_CATALOG = RC.CONSTRAINT_CATALOG
+                 AND KCU1.CONSTRAINT_SCHEMA = RC.CONSTRAINT_SCHEMA
+                 AND KCU1.CONSTRAINT_NAME = RC.CONSTRAINT_NAME
+         JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE KCU2
+              ON KCU2.CONSTRAINT_CATALOG = RC.UNIQUE_CONSTRAINT_CATALOG
+                 AND KCU2.CONSTRAINT_SCHEMA = RC.UNIQUE_CONSTRAINT_SCHEMA
+                 AND KCU2.CONSTRAINT_NAME = RC.UNIQUE_CONSTRAINT_NAME
+
+   WHERE KCU1.ORDINAL_POSITION = KCU2.ORDINAL_POSITION
+         AND KCU1.TABLE_CATALOG = '~a'
+         AND KCU1.CONSTRAINT_CATALOG = '~a'
+         AND KCU1.TABLE_SCHEMA NOT IN ('dto', 'sys')
+
+ORDER BY CONSTRAINT_NAME, KCU1.ORDINAL_POSITION
+"
+                               dbname dbname))
+     :do
+     (let* ((s-entry    (assoc schema result :test 'equal))
+            (t-entry    (when s-entry
+                          (assoc table (cdr s-entry) :test 'equal)))
+            (f-entry    (when t-entry
+                          (assoc name (cdr t-entry) :test 'equal)))
+            (fkey
+             (make-pgsql-fkey :name name
+                              :table-name (qualify-name schema table)
+                              :columns (list col)
+                              :foreign-table (qualify-name fschema ftable)
+                              :foreign-columns (list fcol))))
+       (if s-entry
+           (if t-entry
+               (if f-entry
+                   (let ((fkey (cdr f-entry)))
+                     (push col (pgloader.pgsql::pgsql-fkey-columns fkey))
+                     (push fcol (pgloader.pgsql::pgsql-fkey-foreign-columns fkey)))
+                   (push (cons name fkey) (cdr t-entry)))
+               (push (cons table (list (cons name fkey))) (cdr s-entry)))
+           (push (cons schema
+                       (list (cons table
+                                   (list (cons name fkey))))) result)))
+     :finally
+     ;; we did push, we need to reverse here
+     (return
+       (labels ((reverse-fkey-cols (fkey)
+                  (setf (pgloader.pgsql::pgsql-fkey-columns fkey)
+                        (nreverse (pgloader.pgsql::pgsql-fkey-columns fkey)))
+                  (setf (pgloader.pgsql::pgsql-fkey-foreign-columns fkey)
+                        (nreverse
+                         (pgloader.pgsql::pgsql-fkey-foreign-columns fkey)))
+                  fkey)
+
+                (reverse-fkeys-cols (list-of-fkeys)
+                  (loop :for (name . fkeys) :in list-of-fkeys
+                     :collect (cons name (reverse-fkey-cols fkeys))))
+
+                (reverse-fkeys-cols (list-of-tables)
+                  (reverse
+                   (loop :for (table . fkeys) :in list-of-tables
+                      :collect (cons table (reverse-fkeys-cols fkeys))))))
+         (reverse
+          (loop :for (schema . tables) :in result
+             :collect (cons schema (reverse-fkeys-cols tables))))))))
+
 
 ;;;
 ;;; Tools to handle row queries.

src/sources/mysql/mysql-schema.lisp
@@ -330,52 +330,6 @@ GROUP BY table_name, index_name;"
                            for (name . fks) in schema
                            collect (cons name (reverse fks)))))))
 
-(defun drop-pgsql-fkeys (all-fkeys &key (dbname *pg-dbname*))
-  "Drop all Foreign Key Definitions given, to prepare for a clean run."
-  (let ((all-pgsql-fkeys (list-tables-and-fkeys dbname)))
-    (loop for (table-name . fkeys) in all-fkeys
-       do
-	 (loop for fkey in fkeys
-	    for sql = (format-pgsql-drop-fkey fkey
-					      :all-pgsql-fkeys all-pgsql-fkeys)
-	    when sql
-	    do
-	      (log-message :notice "~a;" sql)
-	      (pgsql-execute sql)))))
-
-(defun create-pgsql-fkeys (all-fkeys
-                           &key
-                             (dbname *pg-dbname*)
-                             state
-                             (label "Foreign Keys"))
-  "Actually create the Foreign Key References that where declared in the
-   MySQL database"
-  (pgstate-add-table state dbname label)
-  (loop for (table-name . fkeys) in all-fkeys
-     do (loop for fkey in fkeys
-	   for sql = (format-pgsql-create-fkey fkey)
-	   do
-	     (log-message :notice "~a;" sql)
-	     (pgsql-execute-with-timing dbname "Foreign Keys" sql state))))
-
-
-;;;
-;;; Sequences
-;;;
-(defun reset-pgsql-sequences (all-columns
-                              &key (dbname *pg-dbname*) state)
-  "Reset all sequences created during this MySQL migration."
-  (let ((tables
-	 (mapcar
-	  (lambda (name) (apply-identifier-case name))
-	  (mapcar #'car all-columns))))
-    (log-message :notice "Reset sequences")
-    (with-stats-collection ("Reset Sequences"
-                            :dbname dbname
-                            :use-result-as-rows t
-                            :state state)
-      (pgloader.pgsql:reset-all-sequences dbname :tables tables))))
-
 
 ;;;
 ;;; Tools to handle row queries, issuing separate is null statements and

commit 8dbb1fe9be114272234a6e59d7702bb1858a662c

Fix bug introduced in f263d1b2a41b8040967a5c5d03351f79beb63d22, fix #124.

src/sources/mysql.lisp
@@ -217,7 +217,7 @@
     ;; while CREATE INDEX statements are in flight (avoid locking).
     ;;
     (when reset-sequences
-      (reset-sequences all-columns :state state))
+      (reset-sequences (mapcar #'car all-columns) :state state))
 
     ;;
     ;; Turn UNIQUE indexes into PRIMARY KEYS now