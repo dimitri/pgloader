54997be2dd628af8dbf004ec0eb42f933446d68e
Victor Kryukov <victor.kryukov@gmail.com>
2015-02-24 16:55:04 -0800

    Fix 182: properly quote tables with . in their names

--- src/pgsql/schema.lisp
+++ src/pgsql/schema.lisp
@@ -26,7 +26,7 @@
           (cond ((quoted-p identifier)
                  :none)
 
-                ((cl-ppcre:scan "^[^A-Za-z_]" identifier)
+                ((cl-ppcre:scan "[^A-Za-z_]" identifier)
                  :quote)
 
                 ((member lowercase-identifier *pgsql-reserved-keywords*
