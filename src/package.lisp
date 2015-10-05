dimitri/pgloader

commit 598c860cf5013d52399c07c7f18f1daf0227d305

Improve user code parsing, fix #297.
To be able to use "t" (or "nil") as a column name, pgloader needs to be
able to generate lisp code where those symbols are available. It's
simple enough in that a Common Lisp package that doesn't :use :cl
fullfills the condition, so intern user symbols in a specially crafted
package that doesn't :use :cl.

Now, we still need to be able to run transformation code that is using
the :cl package symbols and the pgloader.transforms functions too. In
this commit we introduce a heuristic to pick symbols either as functions
from pgloader.transforms or anything else in pgloader.user-symbols.

And so that user code may use NIL too, we provide an override mechanism
to the intern-symbol heuristic and use it only when parsing user code,
not when producing Common Lisp code from the parsed load command.

src/package.lisp
@@ -155,7 +155,7 @@
   (:use #:cl
         #:pgloader.params #:pgloader.utils #:pgloader.connection
         #:pgloader.schema)
-  (:import-from #:pgloader.transforms #:precision #:scale)
+  (:import-from #:pgloader.transforms #:precision #:scale #:intern-symbol)
   (:import-from #:pgloader.parse-date
                 #:parse-date-string
                 #:parse-date-format)

src/parsers/command-csv.lisp
@@ -266,7 +266,7 @@
 (defrule sexp-symbol (and (symbol-first-character-p character)
 			  (* (symbol-character-p character)))
   (:lambda (schars)
-    (pgloader.transforms:intern-symbol (text schars))))
+    (pgloader.transforms:intern-symbol (text schars) '(("nil" . cl:nil)))))
 
 (defrule sexp-string-char (or (not-doublequote character) (and #\\ #\")))
 

src/utils/transforms.lisp
@@ -7,6 +7,44 @@
 
 (in-package :pgloader.transforms)
 
+;;;
+;;; This package is used to generate symbols in the CL code that
+;;; project-fields produces, allowing to use symbols such as t. It's
+;;; important that the user-symbols package doesn't :use cl.
+;;;
+(defpackage #:pgloader.user-symbols (:use))
+
+(defun intern-symbol (symbol-name &optional (overrides '()))
+  "Return a symbol in either PGLOADER.TRANSFORMS if it exists there
+   already (it's a user provided function) or a PGLOADER.USER-SYMBOLS
+   package.
+
+   OVERRIDES is an alist of symbol . value, allowing called to force certain
+   values: the classic example is how to parse the \"nil\" symbol-name.
+   Given OVERRIDES as '((nil . nil)) the returned symbol will be cl:nil
+   rather than pgloader.user-symbols::nil."
+  (let ((overriden (assoc symbol-name overrides :test #'string-equal)))
+    (if overriden
+        (cdr overriden)
+
+        (multiple-value-bind (symbol status)
+            (find-symbol (string-upcase symbol-name)
+                         (find-package "PGLOADER.TRANSFORMS"))
+          ;; pgloader.transforms package (:use :cl) so we might find variable
+          ;; names in there that we want to actually intern in
+          ;; pgloader.user-symbols so that users may use e.g. t as a column name...
+          ;; so only use transform symbol when it denotes a function
+          (cond
+            ((and status (fboundp symbol)) symbol) ; a transform function
+
+            (t
+             (intern (string-upcase symbol-name)
+                     (find-package "PGLOADER.USER-SYMBOLS"))))))))
+
+
+;;;
+;;; Some optimisation stanza
+;;;
 (declaim (inline intern-symbol
 		 zero-dates-to-null
 		 date-with-no-separator
@@ -24,14 +62,6 @@
                  sqlite-timestamp-to-timestamp
                  sql-server-uniqueidentifier-to-uuid
                  sql-server-bit-to-boolean))
-
-
-;;;
-;;; Some tools for reading expressions in the parser, and evaluating them.
-;;;
-(defun intern-symbol (symbol-name)
-  (intern (string-upcase symbol-name)
-	  (find-package "PGLOADER.TRANSFORMS")))
 
 
 ;;;

src/sources/common/project-fields.lisp
@@ -26,8 +26,8 @@
 	   (field-name-as-symbol (field-name-or-list)
 	     "we need to deal with symbols as we generate code"
 	     (typecase field-name-or-list
-	       (list (pgloader.transforms:intern-symbol (car field-name-or-list)))
-	       (t    (pgloader.transforms:intern-symbol field-name-or-list))))
+	       (list (intern-symbol (car field-name-or-list)))
+	       (t    (intern-symbol field-name-or-list))))
 
 	   (process-field (field-name-or-list)
 	     "Given a field entry, return a function dealing with nulls for it"

test/Makefile
@@ -16,6 +16,7 @@ REGRESS= allcols.load 		    \
          csv-keep-extra-blanks.load \
          csv-non-printable.load     \
          csv-nulls.load             \
+         csv-temp.load              \
          csv-trim-extra-blanks.load \
          csv.load 		    \
 	 copy.load                  \

test/csv-temp.load
@@ -1,0 +1,29 @@
+--
+-- See https://github.com/dimitri/pgloader/issues/297
+--
+-- The "t" field would be "temperature", for instance.
+--
+
+LOAD CSV
+    FROM inline (a, b, nil, t)
+    INTO postgresql:///pgloader?temp(a,b,nil,t)
+
+    WITH fields terminated by ';'
+
+  BEFORE LOAD DO
+  $$ drop table if exists temp; $$,
+  $$ CREATE TABLE temp
+      (
+       a   integer,
+       b   timestamp without time zone,
+       nil real,
+       t   real
+      );
+  $$;
+
+
+100;2015-01-01 00:00:00;-6;10
+101;2015-01-02 00:00:00;-2.1;12.5
+102;2015-01-03 00:00:00;3.4;5.5
+103;2015-01-04 00:00:00;4.7;-2.3
+104;2015-01-05 00:00:00;0.4;0

test/regress/expected/csv-temp.out
@@ -1,0 +1,5 @@
+100	2015-01-01 00:00:00	-6	10
+101	2015-01-02 00:00:00	-2.1	12.5
+102	2015-01-03 00:00:00	3.4	5.5
+103	2015-01-04 00:00:00	4.7	-2.3
+104	2015-01-05 00:00:00	0.4	0

commit c880f86bb6cf01f7669390e27a7262f39fdc0931

Fix user defined casting rules.
Commit 598c860cf5013d52399c07c7f18f1daf0227d305 broke user defined
casting rules by interning "precision" and "scale" in the
pgloader.user-symbols package: those symbols need to be found in the
pgloader.transforms package instead.

Luckily enough the infrastructure to do that was already in place for
cl:nil.

src/package.lisp
@@ -6,7 +6,8 @@
   (:use #:cl)
   (:export #:precision
            #:scale
-           #:intern-symbol))
+           #:intern-symbol
+           #:typemod-expr-to-function))
 
 (defpackage #:pgloader.logs
   (:use #:cl #:pgloader.params)
@@ -176,7 +177,11 @@
   (:use #:cl
         #:pgloader.params #:pgloader.utils #:pgloader.connection
         #:pgloader.schema)
-  (:import-from #:pgloader.transforms #:precision #:scale #:intern-symbol)
+  (:import-from #:pgloader.transforms
+                #:precision
+                #:scale
+                #:intern-symbol
+                #:typemod-expr-to-function)
   (:import-from #:pgloader.parse-date
                 #:parse-date-string
                 #:parse-date-format)

src/parsers/command-csv.lisp
@@ -266,7 +266,11 @@
 (defrule sexp-symbol (and (symbol-first-character-p character)
 			  (* (symbol-character-p character)))
   (:lambda (schars)
-    (pgloader.transforms:intern-symbol (text schars) '(("nil" . cl:nil)))))
+    (pgloader.transforms:intern-symbol
+     (text schars)
+     '(("nil"       . cl:nil)
+       ("precision" . pgloader.transforms::precision)
+       ("scale"     . pgloader.transforms::scale)))))
 
 (defrule sexp-string-char (or (not-doublequote character) (and #\\ #\")))
 

src/sources/common/casting-rules.lisp
@@ -29,13 +29,6 @@
 				       :start (+ 1 start-1) :end end))
 	  (cons a b))))))
 
-(defun typemod-expr-to-function (expr)
-  "Transform given EXPR into a callable function object."
-  `(lambda (typemod)
-     (destructuring-bind (precision &optional (scale 0)) typemod
-       (declare (ignorable precision scale))
-       ,expr)))
-
 (defun typemod-expr-matches-p (rule-typemod-expr typemod)
   "Check if an expression such as (< 10) matches given typemod."
   (funcall (compile nil (typemod-expr-to-function rule-typemod-expr)) typemod))

src/utils/transforms.lisp
@@ -41,6 +41,14 @@
              (intern (string-upcase symbol-name)
                      (find-package "PGLOADER.USER-SYMBOLS"))))))))
 
+(defun typemod-expr-to-function (expr)
+  "Transform given EXPR into a callable function object."
+  `(lambda (typemod)
+     (destructuring-bind (precision &optional (scale 0))
+         typemod
+       (declare (ignorable precision scale))
+       ,expr)))
+
 
 ;;;
 ;;; Some optimisation stanza