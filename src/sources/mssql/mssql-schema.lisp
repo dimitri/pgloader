4cb83ec6a593d8868b1477a1ec2a46e30885a134
Dimitri Fontaine <dim@tapoueh.org>
2016-03-16 21:55:40 +0100

    DEBUG mode should list all SQL queries sent.
    Even for MS SQL source.

--- src/sources/mssql/mssql-schema.lisp
+++ src/sources/mssql/mssql-schema.lisp
@@ -39,6 +39,7 @@
 (defun mssql-query (query)
   "Execute given QUERY within the current *connection*, and set proper
    defaults for pgloader."
+  (log-message :debug "MSSQL: sending query: ~a" query)
   (mssql:query query :connection (conn-handle *mssql-db*)))
 
 
