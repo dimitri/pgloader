9a0c50f70026bb808bf0ee68349aea56e8d330ac
Andy Freeland <andy@andyfreeland.net>
2016-12-17 07:30:57 -0800

    Make sure EPEL is enabled when installing SBCL (#494)

--- bootstrap-centos.sh
+++ bootstrap-centos.sh
@@ -5,7 +5,7 @@ sudo yum -y install yum-utils rpmdevtools @"Development Tools" \
 
 # SBCL 1.3, we'll overwrite the repo version of sbcl with a more recent one
 sudo yum -y install epel-release
-sudo yum install -y sbcl.x86_64
+sudo yum install -y sbcl.x86_64 --enablerepo=epel
 
 wget http://downloads.sourceforge.net/project/sbcl/sbcl/1.3.6/sbcl-1.3.6-source.tar.bz2
 tar xfj sbcl-1.3.6-source.tar.bz2
