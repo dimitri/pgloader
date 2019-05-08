ede385bce72f61228529cce0985681a121f5a1a5
Dimitri Fontaine <dim@tapoueh.org>
2019-05-09 00:36:58 +0200

    Upgrade travis testing to SBCL 1.5.2.
    That's needed for current ironclad.

--- .travis.sh
+++ .travis.sh
@@ -15,8 +15,8 @@ lisp_install() {
 			;;
 
 		sbcl)
-			sbcl_checksum='eb44d9efb4389f71c05af0327bab7cd18f8bb221fb13a6e458477a9194853958'
-			sbcl_version='1.3.18'
+			sbcl_checksum='dac6aef3ec7628c284a31f2222ede5d47d9d94f9cfdffe0f00ef40f9531e3c3f'
+			sbcl_version='1.5.2'
 
 			remote_file "/tmp/sbcl-${sbcl_version}.tgz" "http://prdownloads.sourceforge.net/sbcl/sbcl-${sbcl_version}-x86-64-linux-binary.tar.bz2" "$sbcl_checksum"
 			tar --file  "/tmp/sbcl-${sbcl_version}.tgz" --extract --directory '/tmp'
