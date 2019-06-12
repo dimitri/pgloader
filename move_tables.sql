2713ffeeb4d92fc2312b2b5a1eaa47e8e55daef9
wileyj <wileyj@users.noreply.github.com>
2019-06-12 15:26:34 -0400

    no longer needed. address in docker-entrypoint

--- move_tables.sql
+++
@@ -1,8 +0,0 @@
-DO $$DECLARE row record;
-BEGIN
-    FOR row IN SELECT tablename FROM pg_tables WHERE schemaname = 'temp'
-    LOOP
-        EXECUTE 'DROP TABLE IF EXISTS blockstack_core.' || quote_ident(row.tablename) || ' CASCADE;';
-        EXECUTE 'ALTER TABLE temp.' || quote_ident(row.tablename) || ' SET SCHEMA blockstack_core;';
-    END LOOP;
-END$$;
