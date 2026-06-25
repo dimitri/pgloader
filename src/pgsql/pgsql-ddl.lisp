739923f3802ec6fee978708ec672fea26d2ff7bf
Dimitri Fontaine <dim@tapoueh.org>
2026-06-25 22:59:19 +0200

    also omit "DEFAULT NULL" when (eq (column-default column) :NULL) (#1737)
    Co-authored-by: Robert Mayer <mayer_robert@gmx.net>

--- src/pgsql/pgsql-ddl.lisp
+++ src/pgsql/pgsql-ddl.lisp
@@ -156,7 +156,7 @@
             typemod
             typemod
             (column-nullable column)
-            (column-default column)
+            (and (column-default column) (not (eq (column-default column) :NULL)))
             (format-default-value column))))
 
 (defvar *pgsql-default-values*
